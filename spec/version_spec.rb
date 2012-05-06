295594906372a42f777be3edd97b9374779fe75f
Alexander Korsak <alex.korsak@gmail.com>
2012-05-06 03:19:14 +0300

    Change specs for version incrementer

--- spec/version_spec.rb
+++ spec/version_spec.rb
@@ -41,33 +41,31 @@ describe Version do
     end
   end
 
-  context "when we have minor changes and several tags" do
+  context "when we have several tags in the project" do
     let(:repository_path) { './spec/fixtures/tags_repo/dot_git' }
 
-    it "should increment minor version on minor changes" do
-      version.should_receive(:tag_version).with('0.0.3').and_return
+    context "for minor changes" do
+      it "should increment minor version on minor changes" do
+        version.should_receive(:tag_version).with('0.0.3').and_return
 
-      version.minor
+        version.minor
+      end
     end
-  end
 
-  context "when we have major changes and several tags" do
-    let(:repository_path) { './spec/fixtures/tags_repo/dot_git' }
+    context "for major changes" do
+      it "should increment major version and minor as zero" do
+        version.should_receive(:tag_version).with('0.1.0').and_return
 
-    it "should increment major version and minor as zero" do
-      version.should_receive(:tag_version).with('0.1.0').and_return
-
-      version.major
+        version.major
+      end
     end
-  end
 
-  context "when we have release changes and several tags" do
-    let(:repository_path) { './spec/fixtures/tags_repo/dot_git' }
+    context "for release changes" do
+      it "should increment release version and minor, major as zero" do
+        version.should_receive(:tag_version).with('1.0.0').and_return
 
-    it "should increment release version and minor, major as zero" do
-      version.should_receive(:tag_version).with('1.0.0').and_return
-
-      version.release
+        version.release
+      end
     end
   end
 end
