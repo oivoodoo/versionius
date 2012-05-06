d00d9c8a7df797a3029284be52c2b214438928a0
Alexander Korsak <alex.korsak@gmail.com>
2012-05-06 10:36:55 +0300

    Require railtie if we have Rails version more than 3

--- lib/versionius.rb
+++ lib/versionius.rb
@@ -3,7 +3,7 @@ require "versionius/builder"
 require "versionius/tasks"
 require "versionius/version"
 
-require "versionius/railtie" if defined?(Rails)
+require "versionius/railtie" if defined?(Rails) and Rails.version > "3.0.0"
 
 module Versionius
   FILE_NAME = 'version.txt'
